e4be1f88c944c5a775e30d60ea7282650b82baf4
ivanexDev <ivanexdev@gmail.com>
2023-09-06 16:52:23 -0300

    Ejercicio 1.4

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -3,30 +3,26 @@ import PropTypes from "prop-types";
 const App = () => {
   // const-definitions
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -38,9 +34,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part content={props.part1.name} exercises={props.part1.exercises} />
-      <Part content={props.part2.name} exercises={props.part2.exercises} />
-      <Part content={props.part3.name} exercises={props.part3.exercises} />
+      <Part
+        content={props.parts[0].name}
+        exercises={props.parts[0].exercises}
+      />
+      <Part
+        content={props.parts[1].name}
+        exercises={props.parts[1].exercises}
+      />
+      <Part
+        content={props.parts[2].name}
+        exercises={props.parts[2].exercises}
+      />
     </div>
   );
 };
@@ -49,7 +54,9 @@ const Total = (props) => {
   return (
     <div>
       Total de ejercicios :
-      {props.exercises1 + props.exercises2 + props.exercises3}
+      {props.parts[0].exercises +
+        props.parts[1].exercises +
+        props.parts[2].exercises}
     </div>
   );
 };
@@ -68,15 +75,11 @@ Header.propTypes = {
 };
 
 Content.propTypes = {
-  part1: PropTypes.object,
-  part2: PropTypes.object,
-  part3: PropTypes.object,
+  parts: PropTypes.array,
 };
 
 Total.propTypes = {
-  exercises1: PropTypes.number,
-  exercises2: PropTypes.number,
-  exercises3: PropTypes.number,
+  parts: PropTypes.array,
 };
 
 Part.propTypes = {
