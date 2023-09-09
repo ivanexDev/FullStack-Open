e905824f71b51f3143d6106673fcf2bc12251a3e
ivanexDev <ivanexdev@gmail.com>
2023-09-09 13:07:47 -0300

    ejercicio 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -7,29 +7,35 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const avarage = (good - bad) / all;
+  const positive = (good / all) * 100;
 
-  const handleGoodClick = () => {
-    setGood(good + 1);
-  };
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1);
-  };
-
-  const handleBadClick = () => {
-    setBad(bad + 1);
+  const handleClick = (value) => {
+    if (value === "good") {
+      setGood(good + 1);
+    }
+    if (value === "neutral") {
+      setNeutral(neutral + 1);
+    }
+    if (value === "bad") {
+      setBad(bad + 1);
+    }
   };
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <button onClick={handleGoodClick}>Good</button>
-      <button onClick={handleNeutralClick}>Neutral</button>
-      <button onClick={handleBadClick}>Bad</button>
+      <button onClick={() => handleClick("good")}>Good</button>
+      <button onClick={() => handleClick("neutral")}>Neutral</button>
+      <button onClick={() => handleClick("bad")}>Bad</button>
       <h2>Statics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Avarage: {avarage}</p>
+      <p>Positive: {positive}</p>
     </div>
   );
 };
