f86c3217fa420d4105ab1750360f97578479704e
ivanexDev <ivanexdev@gmail.com>
2023-11-20 19:05:41 -0300

    ejercicio 2.2 2.3*

--- part2/courses/src/components/Course.jsx
+++ part2/courses/src/components/Course.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
 const Course = ({course}) => {
+
+    const total = course.parts.reduce((sum, value)=> sum + value.exercises, 0)
+
+
   return (
     <>
     <header>
     <h1>{course.name}</h1>
     </header>
     {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-
+    <p><strong>Total of {total} excercises</strong></p>
     </>
   )
 }
