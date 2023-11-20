055da38ade74a6c08dab0d3667867a823d03ef54
ivanexDev <ivanexdev@gmail.com>
2023-11-20 19:15:35 -0300

    ejercicio 2.4

--- part2/courses/src/components/Course.jsx
+++ part2/courses/src/components/Course.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable react/prop-types */
-const Course = ({course}) => {
-
-    const total = course.parts.reduce((sum, value)=> sum + value.exercises, 0)
-
+const Course = ({ course }) => {
+  const total = course.parts.reduce((sum, value) => sum + value.exercises, 0);
 
   return (
     <>
-    <header>
-    <h1>{course.name}</h1>
-    </header>
-    {course.parts.map(part => <p key={part.id}>{part.name} {part.exercises}</p>)}
-    <p><strong>Total of {total} excercises</strong></p>
+      <header>
+        <h1>{course.name}</h1>
+      </header>
+      {course.parts.map((part) => (
+        <p key={part.id}>
+          {part.name} {part.exercises}
+        </p>
+      ))}
+      <p>
+        <strong>Total of {total} excercises</strong>
+      </p>
     </>
-  )
-}
+  );
+};
 
-export default Course
+export default Course;
