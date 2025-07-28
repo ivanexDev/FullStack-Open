5a270fe70b82273b9465c018917a5cd75df7c6a6
ivanexDev <ivanexdev@gmail.com>
2025-07-28 12:20:47 -0400

    ex 4.2

--- part3/server/.eslintrc.js
+++ part3/server/.eslintrc.js
@@ -47,6 +47,7 @@ module.exports = {
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
+    'no-console': 0,
     indent: [
       'error',
       2
---
+++ part4/controllers/blogs.js
@@ -0,0 +1,45 @@
+const blogsRouter = require("express").Router();
+const { request, response } = require("../app");
+const Blog = require("../models/blog");
+
+blogsRouter.get("/", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.get("/:id", (request, response) => {
+
+  Blog.findById(request.params.id)
+    .then((blog) => {
+      if (blog) {
+        response.json(blog);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.post("/", (request, response, next) => {
+  const blog = new Blog(request.body);
+
+  blog
+    .save()
+    .then((result) => {
+      response.status(201).json(result);
+    })
+    .catch((error) => next(error));
+});
+
+blogsRouter.delete("/:id", (request, response)=>{
+
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+
+})
+
+module.exports = blogsRouter;
