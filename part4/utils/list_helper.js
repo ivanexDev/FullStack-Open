21797e9542b50909d28cf30c703b5af97e5cb65a
ivanexDev <ivanexdev@gmail.com>
2025-07-28 14:05:03 -0400

    ex 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,8 +1,30 @@
 const dummy = (blogs) => {
   // ...
-  return 1
-}
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs
+    .map((blog) => blog.likes)
+    .reduce((acc, cur) => {
+      return acc + cur;
+    });
+
+  if (blogs.length === 1) return blogs[0].likes;
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return {};
+
+  const { title, author, likes } = blogs.reduce((max, current) => {
+    return max.likes < current.likes ? current : max;
+  });
+
+  return { title, author, likes };
+};
 
 module.exports = {
-  dummy
-}
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
